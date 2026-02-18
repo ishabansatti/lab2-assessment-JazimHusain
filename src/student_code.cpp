19aac45e2781bb18becd54934a8704791290cf51
JazimHusain <jazimhussain659@gmail.com>
2026-02-18 14:51:22 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int getIntValue() {
+/*int getIntValue() {
     // Declare a variable var_int of type int and set its value to 10.
     int var_int = 10;
     return var_int;
-}
+} */
 
 float getFloatValue() {
     // Declare a variable var_float of type float and set its value to 10.75.
@@ -15,10 +15,10 @@ float getFloatValue() {
     return var_float;
 }
 
-char getCharValue() {
+/*char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
-    char var_char = 'A';
+ char var_char = 'A';
     return var_char;
 }
 
